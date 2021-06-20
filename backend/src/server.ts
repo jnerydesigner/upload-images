8e075a733c1ccf5470967f143ca9072f5b798661
jnerydesigner <jander.webmaster@gmail.com>
2021-06-20 11:21:22 -0400

    Initial: Initialised Application Backend

---
+++ backend/src/server.ts
@@ -0,0 +1,13 @@
+import express, { Request, Response } from 'express';
+import { routes } from './routes';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use(routes);
+
+
+app.listen(3333, () => console.log("Server is running 🏎️ "));
